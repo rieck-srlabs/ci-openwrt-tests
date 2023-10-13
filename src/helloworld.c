8c64abd0345b1a5ef183a32efc5cccb638cb0f69
Jakob Rieck <rieck@srlabs.de>
2023-10-13 14:58:59 +0200

    Helloworld OpenWRT package

---
+++ src/helloworld.c
@@ -0,0 +1,11 @@
+/****************
+* helloworld.c
+* The most simplistic C program ever written.
+*****************/
+#include <stdio.h>
+
+int main(void)
+{
+        printf("Hello world, when you see this, it means it works!\n\n");
+        return 0;
+}
